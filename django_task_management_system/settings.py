105122a074558e1263afcf05fe079aed6fe757b0
Pramod <single.core.developers@gmail.com>
2024-06-22 21:36:30 +0530

    update 1.1

--- django_task_management_system/settings.py
+++ django_task_management_system/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#kfz(&+ra1by8r1fbqlg=z$3w+@a$iq&89&0w*+(o81@&=n#%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.varcel.app']
 
 # Application definition
 
