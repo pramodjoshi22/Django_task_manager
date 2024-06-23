ade69cde35b88f588f218c73d1009e927a3dfa65
Pramod Joshi <single.core.developers@gmail.com>
2024-06-23 07:46:13 +0530

    Update settings.py

--- django_task_management_system/settings.py
+++ django_task_management_system/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#kfz(&+ra1by8r1fbqlg=z$3w+@a$iq&89&0w*+(o81@&=n#%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-task-manager-three.vercel.app']
+ALLOWED_HOSTS = ['django-task-manager-pe06.onrender.com']
 
 # Application definition
 
